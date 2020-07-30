4bf0e793ba45692e7393e1022bb3616f53c66ef1
rahul-abhishek-au6 <rahulabhishek2june@gmail.com>
2020-07-30 17:44:19 +0530

    Add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-react-rahul
+
+Live at: https://node-react-rahul.herokuapp.com/
--- server/mongodb.js
+++ server/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const fs = require('fs');
 
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb+srv://HMS_Rahul:AttainU123@cluster0-sjhiu.mongodb.net/AttainU');
 
 fs.readdirSync(path.join(__dirname, '/models')).forEach(file => {
   require('./models/' + file);
